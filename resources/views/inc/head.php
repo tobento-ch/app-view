0ca24694fd42c7c1dcbbbef2ce41065b9b22163e
TOBENTOch <info@tobento.ch>
2024-03-23 12:23:58 +0100

    csrf token meta tag added

--- resources/views/inc/head.php
+++ resources/views/inc/head.php
@@ -1,5 +1,9 @@
 <meta name="theme-color" content="#bbfffd">
 
+<?php if ($view->data()->get('csrfToken')) { ?>
+    <meta name="csrf-token" content="<?= $view->esc($view->data()->get('csrfToken')) ?>">
+<?php } ?>
+
 <link rel="shortcut icon" href="favicon.ico">
 
 <?php
