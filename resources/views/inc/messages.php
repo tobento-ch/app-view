b8ac2fe392b0fc1204b978af988f6ba60bdb860a
TOBENTOch <info@tobento.ch>
2024-03-23 12:24:31 +0100

    using message method as not to double escape

--- resources/views/inc/messages.php
+++ resources/views/inc/messages.php
@@ -1,3 +1,3 @@
 <?php foreach($messages as $message) { ?>
-<p class="message <?= $view->esc($message->level()) ?>"><?= $view->esc($message) ?></p>
+<p class="message <?= $view->esc($message->level()) ?>"><?= $view->esc($message->message()) ?></p>
 <?php } ?>
