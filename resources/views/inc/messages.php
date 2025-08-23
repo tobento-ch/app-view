573ef51f81783858c4b7883559234c1c4d9fec6f
TOBENTOch <info@tobento.ch>
2025-08-23 14:51:14 +0200

    data-message attribute added

--- resources/views/inc/messages.php
+++ resources/views/inc/messages.php
@@ -1,3 +1,3 @@
 <?php foreach($messages as $message) { ?>
-<p class="message <?= $view->esc($message->level()) ?>"><?= $view->esc($message->message()) ?></p>
+<p data-message="<?= $view->esc($message->level()) ?>" class="message <?= $view->esc($message->level()) ?>"><?= $view->esc($message->message()) ?></p>
 <?php } ?>
